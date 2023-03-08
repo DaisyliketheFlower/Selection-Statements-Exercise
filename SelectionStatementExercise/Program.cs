8876dc653f3073a0b18ec9b431d776bf9e59b3c8
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-08 17:16:53 -0500

    done

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var faveNumber = r.Next(1, 10);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < faveNumber)
+            {
+                Console.WriteLine("The variable is set to tooLow.");
+            }
+            else if (userInput > faveNumber)
+            {
+                Console.WriteLine("The variable is set to tooHigh");
+            }
+              else
+            {
+                Console.WriteLine("The variable is set to youGuessedIt");
+            }
         }
     }
 }
